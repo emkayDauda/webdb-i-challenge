fa03755dd2b413f726b436e91d709fb0e03cc997
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-04 14:58:37 +0100

    :sparkles: Create ID validator middleware for accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -7,11 +7,27 @@ const accounts = express.Router();
 accounts.get("/", (req, res) => {
   db("accounts")
     .then(accounts => res.status(201).json(accounts))
-    .catch(err =>
-      res
-        .status(500)
-        .json({ error: true, message: "Error occurred with server" })
-    );
+    .catch(err => res.status(500).json({ error: true, message: err.message }));
 });
+
+
+function idValidator(req, res, next) {
+  const { id } = req.params;
+  if (id) {
+    db("accounts")
+      .where("accounts.id", id)
+      .first()
+      .then(account => {
+        if (account) {
+          req.valAccount = account;
+          next();
+        } else {
+          res
+            .status(404)
+            .json({ error: true, message: "Account with that ID not found" });
+        }
+      });
+  }
+}
 
 module.exports = accounts;
