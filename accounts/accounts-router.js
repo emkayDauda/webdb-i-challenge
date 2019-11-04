d1203849bb72b87f00ec1f5e0d17705440e51546
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-04 15:41:26 +0100

    :truck: Reformat code

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -14,24 +14,6 @@ accounts.get("/:id", idValidator, (req, res) => {
   res.status(201).json(req.valAccount);
 });
 
-function idValidator(req, res, next) {
-  const { id } = req.params;
-  if (id) {
-    db("accounts")
-      .where("accounts.id", id)
-      .first()
-      .then(account => {
-        if (account) {
-          req.valAccount = account;
-          next();
-        } else {
-          res
-            .status(404)
-            .json({ error: true, message: "Account with that ID not found" });
-        }
-      });
-  }
-}
 
 accounts.post("/", accountBodyValidator, (req, res) => {
   db("accounts")
@@ -67,6 +49,25 @@ accounts.put("/:id", idValidator, accountBodyValidator, (req, res) => {
     .catch(err => res.status(500).json({ error: true, message: err.message }));
 });
 
+
+function idValidator(req, res, next) {
+    const { id } = req.params;
+    if (id) {
+      db("accounts")
+        .where("accounts.id", id)
+        .first()
+        .then(account => {
+          if (account) {
+            req.valAccount = account;
+            next();
+          } else {
+            res
+              .status(404)
+              .json({ error: true, message: "Account with that ID not found" });
+          }
+        });
+    }
+  }
 function accountBodyValidator(req, res, next) {
   const { name, budget } = req.body;
 
